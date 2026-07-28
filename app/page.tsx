8ae61f206d41fc7952a2d0ec1b40f413ef59db35
Ijeh Joel <cijeh.2404322@stu.cu.edu.ng>
2026-07-28 20:27:54 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,10 @@ export default async function HomePage() {
       <AnnouncementBanner />
       <CategoryBar />
 
-      
+      {/* Promotional Hero Section */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+
           {/* Right: Promotional Cards */}
           <div className="flex flex-col gap-6">
             {/* Hungry Card */}
