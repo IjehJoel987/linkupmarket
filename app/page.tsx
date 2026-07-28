7db869a18ebb143dbfee996a5e5ae820ab9162ee
Ijeh Joel <cijeh.2404322@stu.cu.edu.ng>
2026-07-28 20:40:05 +0100

    Remove promotional hero section from page
    Removed the promotional hero section from the page.

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,7 @@ export default async function HomePage() {
       <AnnouncementBanner />
       <CategoryBar />
 
-      {/* Promotional Hero Section */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
 
-          
-
-
-          </div>
-        </div>
-      </div>
 
       {/* Benefits Section */}
       <div className="bg-gray-50 py-8">
